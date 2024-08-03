ea13b8335234ca4de3091f78dc183d1d88b82358
henriits <henritsarents@hotmail.com>
2024-08-03 22:02:03 +0300

    fix: connect categories with router

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -2,11 +2,13 @@ import { router } from "@server/trpc";
 import users from "./users";
 import projects from "./projects";
 import comments from "./comments";
+import categories from "./categories";
 
 export const appRouter = router({
     users,
     projects,
     comments,
+    categories,
 });
 
 export type AppRouter = typeof appRouter;

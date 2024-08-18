36a470b6cc73265c17e17c18ebfa3a197ad0f061
henriits <henritsarents@hotmail.com>
2024-08-18 14:40:29 +0300

    fix: trying to resolve testdb issue

--- server/src/tests/utils/database.ts
+++ server/src/tests/utils/database.ts
@@ -1,4 +1,11 @@
 import config from "@server/config";
 import { createDatabase } from "@server/database";
 
-export const createTestDatabase = () => createDatabase(config.testDatabase);
+export const createTestDatabase = () => {
+    if (!config.testDatabase) {
+        console.warn("Test database configuration is not provided.");
+        return undefined;
+    }
+
+    return createDatabase(config.testDatabase);
+};
